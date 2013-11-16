896ffd6149d04596844cda0485897eae445da155
xpwang <xianglewang@Xiangle-Wangs-MacBook-Pro.local>
2013-11-16 16:52:50 -0600

    Gesture Implementation
    - added Gesture
    - added ScoreBoardActivity

--- AngrySimon/gen/edu/iastate/angrysimon/R.java
+++ AngrySimon/gen/edu/iastate/angrysimon/R.java
@@ -33,15 +33,18 @@ public final class R {
         public static final int button6=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_simon=0x7f030000;
+        public static final int activity_score_board=0x7f030000;
+        public static final int activity_simon=0x7f030001;
     }
     public static final class menu {
-        public static final int simon=0x7f070000;
+        public static final int score_board=0x7f070000;
+        public static final int simon=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_score_board=0x7f050003;
     }
     public static final class style {
         /** 
