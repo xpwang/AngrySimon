78611649ec49ea0f46a3c1a9e8ee9444219d114c
raj <raj@raj-Aspire-M5-583P>
2013-11-21 18:52:54 -0600

    testing exporting main screen testing

--- AngrySimon/gen/edu/iastate/angrysimon/R.java
+++ AngrySimon/gen/edu/iastate/angrysimon/R.java
@@ -17,34 +17,40 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f080001;
-        public static final int button3=0x7f080002;
-        public static final int button4=0x7f080003;
-        public static final int button5=0x7f080004;
-        public static final int button6=0x7f080005;
+        public static final int action_settings=0x7f090009;
+        public static final int button1=0x7f090003;
+        public static final int button2=0x7f090004;
+        public static final int button3=0x7f090005;
+        public static final int button4=0x7f090006;
+        public static final int button5=0x7f090007;
+        public static final int button6=0x7f090008;
+        public static final int button_Angry_Simon=0x7f090001;
+        public static final int button_Classic_Simon=0x7f090000;
+        public static final int button_Rage_Simon=0x7f090002;
     }
     public static final class layout {
-        public static final int activity_score_board=0x7f030000;
-        public static final int activity_simon=0x7f030001;
+        public static final int activity_main_screen=0x7f030000;
+        public static final int activity_score_board=0x7f030001;
+        public static final int activity_settings_screen=0x7f030002;
+        public static final int activity_simon=0x7f030003;
     }
     public static final class menu {
-        public static final int score_board=0x7f070000;
-        public static final int simon=0x7f070001;
+        public static final int score_board=0x7f080000;
+        public static final int simon=0x7f080001;
+        public static final int simon_main_screen_menu=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_score_board=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_score_board=0x7f060003;
     }
     public static final class style {
         /** 
@@ -68,10 +74,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int activity_main_screen=0x7f040000;
     }
 }
