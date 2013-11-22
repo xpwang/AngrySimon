8f50531561a4629e877eb4bc165ce3d536e30619
raj <raj@raj-Aspire-M5-583P>
2013-11-21 19:58:40 -0600

    Main Screen

--- AngrySimon/gen/edu/iastate/angrysimon/R.java
+++ AngrySimon/gen/edu/iastate/angrysimon/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
+        public static final int RelativeLayout1=0x7f090009;
+        public static final int action_settings=0x7f090011;
         public static final int button1=0x7f090003;
         public static final int button2=0x7f090004;
         public static final int button3=0x7f090005;
@@ -34,12 +35,20 @@ public final class R {
         public static final int button_Angry_Simon=0x7f090001;
         public static final int button_Classic_Simon=0x7f090000;
         public static final int button_Rage_Simon=0x7f090002;
+        public static final int done_button=0x7f09000f;
+        public static final int game_over_text=0x7f09000a;
+        public static final int name_text_box=0x7f09000e;
+        public static final int play_again_button=0x7f090010;
+        public static final int score_layout=0x7f09000b;
+        public static final int score_num_text=0x7f09000d;
+        public static final int score_text=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_main_screen=0x7f030000;
         public static final int activity_score_board=0x7f030001;
         public static final int activity_settings_screen=0x7f030002;
         public static final int activity_simon=0x7f030003;
+        public static final int game_finished_dialog=0x7f030004;
     }
     public static final class menu {
         public static final int score_board=0x7f080000;
@@ -49,7 +58,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int done_button=0x7f060006;
+        public static final int game_over=0x7f060004;
         public static final int hello_world=0x7f060002;
+        public static final int play_again_button=0x7f060007;
+        public static final int score=0x7f060005;
         public static final int title_activity_score_board=0x7f060003;
     }
     public static final class style {
