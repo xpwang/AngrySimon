048bb5f0cf94a98d35662fc474d6ed86e2d9e64f
ericvoje <ericvoje@gmail.com>
2013-12-07 01:01:14 -0600

    commit again

--- AngrySimon/src/edu/iastate/angrysimon/SimonActivity.java
+++ AngrySimon/src/edu/iastate/angrysimon/SimonActivity.java
@@ -15,8 +15,6 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
-//hi eric
-//hi peter
 
 public class SimonActivity extends CustomGestureListener {
 
